a437a53294f8fe9514f9bd788dc2a33cf1a9bc6e
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2023-09-05 18:20:45 +0000

    all

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 10-add.py
+# vanessa umugwaneza <umugwanezavanessa4@gmail.com>
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
+    
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 11-pow.py
+# vanessa umugwaneza <umugwanezavanessa4@gmail.com>
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
