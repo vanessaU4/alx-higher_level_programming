0e96b785aed10bd54ed0fc54864c7c96ff13b9b4
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2023-09-25 22:33:42 +0000

    all

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-raise_exception.py
+# Umugwaneza vanessa <umugwanezavanessa4@gmail.com>
+
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
