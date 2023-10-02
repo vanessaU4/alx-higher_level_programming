db40f7f7e6a3d952195767cee0774b2948ca9edc
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2023-10-02 16:41:51 +0000

    all

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Vanessa Umugwaneza <umugwanezavanessa4@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
