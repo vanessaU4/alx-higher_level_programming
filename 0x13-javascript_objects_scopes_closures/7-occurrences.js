edc3ac4e2cc66b7fc5b569a440e723ae0426f338
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2023-12-12 23:49:06 +0000

    all

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const nbOccurences = function (list, searchElement) {
+    return list.filter(el => el === searchElement).length;
+  };
+  
+  exports.nbOccurences = nbOccurences;
+  
